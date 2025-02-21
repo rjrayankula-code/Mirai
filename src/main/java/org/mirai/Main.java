2a288b161e599146caef832c22349d93009e85eb
rjrayankula-code <rajrayankula@gmail.com>
2025-02-21 13:45:59 -0500

    Rename

--- src/main/java/org/mirai/Main.java
+++ src/main/java/org/mirai/Main.java
@@ -1,11 +1,11 @@
-package org.example;
+package org.mirai;
 
    import org.springframework.boot.SpringApplication;
    import org.springframework.boot.autoconfigure.SpringBootApplication;
    import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("org.example.controller")
+@ComponentScan("org.mirai.controller")
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/org/mirai/controller/GreetingController.java
+++ src/main/java/org/mirai/controller/GreetingController.java
@@ -1,4 +1,4 @@
-   package org.example.controller;
+   package org.mirai.controller;
 
    import org.springframework.web.bind.annotation.GetMapping;
    import org.springframework.web.bind.annotation.RequestMapping;
